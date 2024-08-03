a68c001ca0e33e3469826144899991cb8275a189
Hamm <majhamm@qq.com>
2024-08-03 14:13:25 +0800

    重构代码库以支持多算法加密解密
    重构了现有的代码库，使其能够支持多种加密解密算法，而不仅仅是AES。此更改涉及到去除特定AES逻辑，引入更广泛的加密算法支持，并确保代码库可以灵活地适应不同的加密需求。同时，更新了相关测试以验证新架构的正确性。
    
    BREAKING CHANGE: 对于依赖特定AES实现的客户端，将需要更新以适应新的多算法支持。
    
    Signed-off-by: Hamm <majhamm@qq.com>

---
+++ src/main/java/cn/hamm/sdk/common/base/AbstractResponse.java
@@ -0,0 +1,17 @@
+package cn.hamm.sdk.common.base;
+
+/**
+ * <h1>响应基模型</h1>
+ *
+ * @param <R> 响应模型
+ * @author Hamm.cn
+ */
+public abstract class AbstractResponse<R extends AbstractResponse<R>> {
+    /**
+     * <h2>解析数据</h2>
+     *
+     * @param data 解密后的data数据
+     * @return 解析后的数据
+     */
+    public abstract R parseData(String data);
+}
